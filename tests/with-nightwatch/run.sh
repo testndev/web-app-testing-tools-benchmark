f0de3a08ff384577b31a97bb00e104594d7c0b06
Alhusaine NEMER <al@testndev.com>
2023-04-28 00:51:40 +0200

    logs duration in CSV file

--- tests/with-nightwatch/run.sh
+++ tests/with-nightwatch/run.sh
@@ -4,4 +4,7 @@ echo "##[$runner] ▶ start running all tests"
 start=$(date +%s%N)
 npx nightwatch --headless
 end=$(date +%s%N)
-echo "##[$runner] ◼ Elapsed time: $(($(($end-$start))/1000000)) ms"
+elapsed_time="$(($(($end-$start))/1000000))"
+elapsed_time_log_message="##[$runner] ◼ Elapsed time: $elapsed_time ms"
+echo $elapsed_time_log_message
+echo "\"$runner\";$elapsed_time;\"$(date --iso-8601=seconds)\"" >> ../chrono.csv
