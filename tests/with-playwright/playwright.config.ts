67321b42532f7419c2463accd78eb03b10ec132d
Alhusaine NEMER <al@testndev.com>
2023-05-09 09:47:06 +0200

    set base URL in Playwright config

--- tests/with-playwright/playwright.config.ts
+++ tests/with-playwright/playwright.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
- 
-    use: { ...devices['Desktop Chrome'], channel: 'chrome' }, // or 'chrome-beta'
+
+    use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        baseURL: 'https://the-internet.herokuapp.com'
+    },
     reporter: [['list', { outputFolder: 'test-report' }]],
 });
