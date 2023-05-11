8faa6b823279746ad58ecdf68bc9027138825b9c
Alhusaine NEMER <al@testndev.com>
2023-05-11 10:38:00 +0200

    test: reformat cypress implementation

--- tests/with-cypressio/tests/web/textEditor.spec.ts
+++ tests/with-cypressio/tests/web/textEditor.spec.ts
@@ -1,8 +1,8 @@
 const iframeLocator = 'iframe[title="Rich Text Area"]';
-
+const richTextAreaLocator = '#tinymce'; // vaild inside the iframe
 const initialText = 'Your content goes here.';
 const textFirstParagraph = 'Hello! this should be the title of our text.';
-const textMiddleParagraph = 'This is a paragraphin the middle';
+const textMiddleParagraph = 'This is a paragraph in the middle of the text.';
 const textLastParagraph = 'This is the last paragraph. Good bye!';
 const newText = [textFirstParagraph, textMiddleParagraph, textLastParagraph].join('\n');
 
@@ -17,7 +17,6 @@ describe('The-Internet WYSIWYG editor', () => {
             .its('0.contentDocument').should('not.be.empty')
             .its('body')
             .as('body')
-
         cy.get('@body')
             .should('be.visible')
             .should('not.be.empty')
@@ -25,61 +24,54 @@ describe('The-Internet WYSIWYG editor', () => {
     });
 
     it('should have an initial text [T_20]', () => {
-
         cy.get(iframeLocator)
-            .should(iframe => expect(iframe.contents().find('body')).to.exist)
-            .then(iframe => cy.wrap(iframe.contents().find('body')))
-            .within(($body) => {
+            .then(iframe => cy.wrap(iframe.contents().find(richTextAreaLocator)))
+            .within((_) => {
                 cy.get('p').should('include.text', initialText);
             });
     });
 
     it('should be able to replace initial text by new one [T_21]', () => {
         cy.get(iframeLocator)
-            .should(iframe => expect(iframe.contents().find('body')).to.exist)
             .then(iframe => {
-                const iframeContent = iframe.contents();
-                cy.wrap(iframeContent.find('body'))
+                cy.wrap(iframe.contents().find(richTextAreaLocator))
                     .wait(littlePause)
                     .type('{ctrl}A')
                     .wait(littlePause)
                     .type(newText)
-                    .within(($body) => {
-                        cy.contains('p', textFirstParagraph).should('be.visible')
-                        cy.contains('p', textLastParagraph).should('be.visible')
-                        cy.contains('p', initialText).should('not.be.exist')
+                    .within((_) => {
+                        cy.contains(textFirstParagraph).should('be.visible')
+                        cy.contains(textLastParagraph).should('be.visible')
+                        cy.contains(initialText).should('not.exist')
                     });
-
             });
     });
 
-    it.skip('should be able to delete last paragraph with keyboard shortcuts [T_22]', () => {
+    it('should be able to delete last paragraph with keyboard shortcuts [T_22]', () => {
         cy.get(iframeLocator)
-        .should(iframe => expect(iframe.contents().find('body')).to.exist)
-        .then(iframe => {
-            const iframeContent = iframe.contents();
-            cy.wrap(iframeContent.find('body'))
-                .wait(littlePause)
-                .type('{ctrl}A')
-                .wait(littlePause)
-                .type(newText)
-                .within(($body) => {
-                    cy.contains('p', textFirstParagraph).should('be.visible')
-                    cy.contains('p', textLastParagraph).should('be.visible')
-                    cy.contains('p', initialText).should('not.be.exist')
-                })
-                .type('{end}')
-                .wait(littlePause)
-                .type('{ctrl+shift+uparrow}')
-                .wait(littlePause)
-                .type('{del}')
-                .wait(littlePause)
-                .within(($body) => {
-                    cy.contains('p', textFirstParagraph).should('be.visible')
-                    cy.contains('p', textLastParagraph).should('not.exist')
-                })
-
-        });
+            .then(iframe => {
+                cy.wrap(iframe.contents().find(richTextAreaLocator))
+                    .wait(littlePause)
+                    .type('{ctrl}A')
+                    .wait(littlePause)
+                    .type(newText)
+                    .within((_) => {
+                        cy.contains(textFirstParagraph).should('be.visible')
+                        cy.contains(textLastParagraph).should('be.visible')
+                        cy.contains(initialText).should('not.exist')
+                    })
+                    .type('{end}')
+                    .wait(littlePause)
+                    .type('{ctrl+shift+uparrow}')
+                    .wait(littlePause)
+                    .type('{del}')
+                    .wait(littlePause)
+                    .within((_) => {
+                        cy.contains(textFirstParagraph).should('be.visible')
+                        //FIXME: last paragraph still exists
+                        cy.contains(textLastParagraph).should('not.exist')
+                    })
+            });
     });
 
 });
