358e88264a6b52548b49a9a943b6707ad3e7d4c8
Alhusaine NEMER <al@testndev.com>
2023-05-11 11:18:11 +0200

    fix: refactor some tests

--- tests/with-playwright/tests/web/textEditor.spec.ts
+++ tests/with-playwright/tests/web/textEditor.spec.ts
@@ -1,47 +1,47 @@
 import { test, expect } from '@playwright/test';
 
+const iframeLocator = 'iframe[title="Rich Text Area"]';
+const richTextAreaLocator = '#tinymce'; // vaild inside the iframe
 const initialText = 'Your content goes here.';
-
 const textFirstParagraph = 'Hello! this should be the title of our text.';
-const textMiddleParagraph = 'This is a paragraphin the middle';
+const textMiddleParagraph = 'This is a paragraph in the middle of the text.';
 const textLastParagraph = 'This is the last paragraph. Good bye!';
-
-const newText = [textFirstParagraph , textMiddleParagraph , textLastParagraph].join('\n');
+const newText = [textFirstParagraph, textMiddleParagraph, textLastParagraph].join('\n');
 
 test.describe('The-Internet WYSIWYG editor', async () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('/tinymce');
         await expect(page.locator('h3')).toHaveText('An iFrame containing the TinyMCE WYSIWYG Editor');
+        await expect(page.locator(iframeLocator)).toBeVisible()
     });
 
-    test('should have an initial text [T_20]', async ({ page }) => { 
-        const richTextAreaFrame = page.frameLocator('iframe[title="Rich Text Area"]'); 
-        await expect(richTextAreaFrame.getByText(initialText)).toBeVisible();
+    test('should have an initial text [T_20]', async ({ page }) => {
+        await expect(page.frameLocator(iframeLocator).getByText(initialText)).toBeVisible();
     });
 
-    test('should be able to replace initial text by new one [T_21]', async ({ page }) => { 
-        const richTextAreaFrame = page.frameLocator('iframe[title="Rich Text Area"]');
-        const tinymce = richTextAreaFrame.locator('#tinymce');
-        await expect(richTextAreaFrame.getByText(initialText)).toBeVisible();
+    test('should be able to replace initial text by new one [T_21]', async ({ page }) => {
+        const iframe = page.frameLocator(iframeLocator);
+        const tinymce = iframe.locator(richTextAreaLocator);
+        await expect(iframe.getByText(initialText)).toBeVisible();
         await tinymce.press('Control+a');
         await tinymce.fill(newText);
         await expect(tinymce.getByText(textFirstParagraph)).toBeVisible();
         await expect(tinymce.getByText(textLastParagraph)).toBeVisible();
-        await expect(tinymce.getByText(initialText)).not.toBeVisible();  
+        await expect(tinymce.getByText(initialText)).not.toBeVisible();
     });
 
-    test('should be able to delete last paragraph with keyboard shortcuts [T_22]', async ({ page }) => { 
-        const richTextAreaFrame = page.frameLocator('iframe[title="Rich Text Area"]');
-        const tinymce = richTextAreaFrame.locator('#tinymce');
+    test('should be able to delete last paragraph with keyboard shortcuts [T_22]', async ({ page }) => {
+        const richTextAreaFrame = page.frameLocator(iframeLocator);
+        const tinymce = richTextAreaFrame.locator(richTextAreaLocator);
         await expect(richTextAreaFrame.getByText(initialText)).toBeVisible();
         await tinymce.press('Control+a');
         await tinymce.fill(newText);
         await expect(tinymce.getByText(textLastParagraph)).toBeVisible();
-        await expect(richTextAreaFrame.getByText(initialText)).not.toBeVisible(); 
+        await expect(richTextAreaFrame.getByText(initialText)).not.toBeVisible();
         await tinymce.press('End');
         await tinymce.press('Control+Shift+ArrowUp');
-        await tinymce.press('Delete'); 
+        await tinymce.press('Delete');
         await expect(tinymce.getByText(textFirstParagraph)).toBeVisible();
         await expect(tinymce.getByText(textLastParagraph)).not.toBeVisible();
     });
--- tests/with-selenium-jest/tests/web/simpleFormLogin.spec.ts
+++ tests/with-selenium-jest/tests/web/simpleFormLogin.spec.ts
@@ -1,4 +1,4 @@
-import { Builder, By, until, WebDriver } from 'selenium-webdriver';
+import { Builder, By, WebDriver } from 'selenium-webdriver';
 import { Options } from 'selenium-webdriver/chrome'
 import { describe, expect, test, beforeAll, afterAll, beforeEach } from '@jest/globals';
 
@@ -18,7 +18,7 @@ describe('The-Internet', () => {
         await driver.findElement(By.name('password')).sendKeys('SuperSecretPassword!');
         await driver.findElement(By.css('button[type="submit"]')).click();
         const message = await driver.findElement(By.id('flash')).getText();
-        expect(message).toContain('You logged into a secure area!');
+        await expect(message).toContain('You logged into a secure area!');
     });
 
     test('should display error message for incorrect username [T_11]', async () => {
@@ -26,16 +26,16 @@ describe('The-Internet', () => {
         await driver.findElement(By.name('password')).sendKeys('pa55w0rd');
         await driver.findElement(By.css('button[type="submit"]')).click();
         const message = await driver.findElement(By.id('flash')).getText();
-        expect(message).toContain('Your username is invalid!');
+        await expect(message).toContain('Your username is invalid!');
     });
 
 
     test('should display error message for incorrect password [T_12]', async () => {
         await driver.findElement(By.name('username')).sendKeys('tomsmith');
-        await driver.findElement(By.name('password')).sendKeys('pa55wincorrect_password0rd');
+        await driver.findElement(By.name('password')).sendKeys('incorrect_password');
         await driver.findElement(By.css('button[type="submit"]')).click();
         const message = await driver.findElement(By.id('flash')).getText();
-        expect(message).toContain('Your password is invalid!');
+        await expect(message).toContain('Your password is invalid!');
     });
 
     afterAll(async () => { await driver.quit(); });
