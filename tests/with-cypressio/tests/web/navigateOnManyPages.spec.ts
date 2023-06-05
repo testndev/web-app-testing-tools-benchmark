7f73ee9cff24a0782ea11a7fb0f5fa47c1f882df
Alhusaine NEMER <al@testndev.com>
2023-06-05 09:22:01 +0000

    add implem of S_3 suite in Cypress

--- tests/with-cypressio/tests/web/navigateOnManyPages.spec.ts
+++ tests/with-cypressio/tests/web/navigateOnManyPages.spec.ts
@@ -1,1 +1,33 @@
-//TODO:  implement
+// TODO:  test it
+import listOfLinks from "../../../listOfLinks";
+
+describe('The-Internet, navigate to pages [S_3]', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        cy.get('h1').should('have.text', 'Welcome to the-internet');
+    });
+
+    // copy of old test
+    it('should accept user with valid credentials [T_10]', () => {
+        cy.get('label').contains('Username').siblings('input').type('tomsmith');
+        cy.get('label').contains('Password').siblings('input').type('SuperSecretPassword!');
+        cy.get('button').contains('Login').click();
+        cy.contains('You logged into a secure area!').should('be.visible');
+    }); 
+
+    for (const [i, { targetPageTitlePartialText, linkUrl, linkText }] of listOfLinks.entries()) {
+        const testId = 300 + i;
+        it(`should navigate to "${linkUrl}" page by clicking on link [T_${testId}]`, async () => {
+            cy.get(`a[href="${linkUrl}"]`).should('be.visible');
+            cy.get(`a[href="${linkUrl}"]`).should('have.text', linkText);
+            cy.get(`a[href="${linkUrl}"]`).click();
+            cy.url().should('eq', linkUrl);
+            cy.get('heading').should('be.visible');
+            cy.get('heading').should('contain.text', targetPageTitlePartialText);
+        });
+
+    }
+
+    
+});
