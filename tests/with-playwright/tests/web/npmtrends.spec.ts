c16ce32996e7c60466f8c8f23d905c50253616b7
Alhusaine NEMER <al@testndev.com>
2023-06-11 01:05:03 +0200

    fix: scrap NPM trends

--- tests/with-playwright/tests/web/npmtrends.spec.ts
+++ tests/with-playwright/tests/web/npmtrends.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from '@playwright/test';
 import * as fs from 'fs';
 
 const toolsList = ['cypress', 'nightwatch', 'playwright', 'selenium-webdriver'];
-const periodsList = ['1 Year', '2 Years', '5 Years', 'All time'];
+
+const url = `https://www.npmtrends.com/${toolsList.join('-vs-')}`;
+
 const periodsTitles = {
     '1 Year': 'over the past year',
     '2 Years': 'over the past 2 years',
@@ -10,12 +12,15 @@ const periodsTitles = {
     'All time': 'since beginning'
 }
 
-const screenshotPath = (period)=> `./results/screenshots/NPM-trends ${toolsList.join('.')} - ${period}.png`; 
+const periodsList = Object.keys(periodsTitles);
+
+const screenshotPath = (period) => `./results/screenshots/NPM-trends ${toolsList.join('.')} - ${period}.png`;
 
 test('get NPM trends of those tools from npmtrends.com', async ({ page }) => {
-    let markdown = '';
+    let markdown = `# NPM trends - ${toolsList.join('/')}\n\n`;
+    markdown += `From [npm trends](${url})\n\n`;
     const waitForResponses = toolsList.map(tool => page.waitForResponse(RegExp('.*' + tool + '.*')));
-    const url = 'https://www.npmtrends.com/' + toolsList.join('-vs-');
+    
     await page.goto(url);
     await expect(page).toHaveTitle(/.*NPM Trends.*/i);
     for (const period of periodsList) {
@@ -23,10 +28,10 @@ test('get NPM trends of those tools from npmtrends.com', async ({ page }) => {
             ...waitForResponses,
             page.waitForTimeout(2000), // wait for chart to render
             page.locator('select').selectOption({ label: period })
-        ]); 
+        ]);
         markdown += `## NPM trend - ${periodsTitles[period]}\n\n`;
         markdown += `![${period}](${screenshotPath(period)})\n\n`;
         await page.locator('#download_chart').screenshot({ path: screenshotPath(period) });
-    } 
+    }
     fs.writeFileSync('./results/NPM-trends.md', markdown);
 });
